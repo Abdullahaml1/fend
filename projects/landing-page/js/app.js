379ab65e5094bf5fc860b047d5e7a6baa5ff80e8
Abdullahaml1 <abdullah_aml@protonmail.com>
2020-11-23 22:27:07 +0200

    feat: Adding go to anchor by events

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -36,6 +36,33 @@ function isElementNearTopOfViewPort(element) {
 }
 
 
+/**
+ * @description it calculates the absolute position of an object element
+ *              it was taken from here: https://www.aspsnippets.com/Articles/Get-Absolute-Position-Screen-Cordinates-of-HTML-Elements-using-JavaScript.aspx
+ * @constructor 
+ * @param obj the element object 
+ * @return the position of the  object
+ **/
+
+
+function getAbsolutePosition(obj) {
+    let p = P={};
+    p.x = obj.offsetLeft;
+    p.y = obj.offsetTop;
+    while (obj.offsetParent) {
+        p.x = p.x + obj.offsetParent.offsetLeft;
+        p.y = p.y + obj.offsetParent.offsetTop;
+        if (obj == document.getElementsByTagName("body")[0]) {
+            break;
+        }
+        else {
+            obj = obj.offsetParent;
+        }
+    }
+    return p;
+
+}
+
 
 
 /**
@@ -68,7 +95,7 @@ function buildNav(sectionsList) {
 
         a.textContent = sectionName;
         a.setAttribute('class', 'menu__link'); //adding class name to apply css
-        a.setAttribute('href', `#${sectionId}`); // anchor to every section
+        // a.setAttribute('href', `#${sectionId}`); // anchor to every section
 
         li.appendChild(a);
         frag.appendChild(li);
@@ -79,11 +106,15 @@ function buildNav(sectionsList) {
 }
 
 
-buildNav(sectionsList);
 
 
 // Add class 'active' to section when near top of viewport
 
+/**
+  * @description adding lisetening event when all sections of sectionsList in
+  *              viewport
+  * @constructor
+ **/
 function activateSectionInNearViewport() {
 
     let lastSectionIndex = 0;
@@ -112,6 +143,36 @@ function activateSectionInNearViewport() {
 // Scroll to anchor ID using scrollTO event
 
 
+
+/**
+  * @description scrolls to an element by absolute position
+  * constructor
+  * @param element the element to scroll to
+ **/
+function scrollToElelement(element) {
+    const p = getAbsolutePosition(element);
+    const header = document.querySelector('.page__header');
+    p.y -= header.offsetHeight;
+    scrollTo(p.x, p.y);
+}
+
+
+
+/**
+  * @description adding listening events if a click occurred on the nabvar menu
+  * @constructor
+ **/
+function goToAnchorEvents() {
+    const navLiList = document.querySelectorAll('#navbar__list li');
+
+    for (let i = 0; i <  navLiList.length; i++) {
+        navLiList[i].addEventListener('click', function () {
+            scrollToElelement(sectionsList[i]);
+        });
+    }
+
+}
+
 /**
  * End Main Functions
  * Begin Events
@@ -119,8 +180,10 @@ function activateSectionInNearViewport() {
 */
 
 // Build menu 
+buildNav(sectionsList);
 
 // Scroll to section on link click
+goToAnchorEvents();
 
 // Set sections as active
 document.addEventListener('scroll', activateSectionInNearViewport);
