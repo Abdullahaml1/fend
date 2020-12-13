74442cbac027a1795e3f3f12006a543fb1362d67
Abdullahaml1 <abdullah_aml@protonmail.com>
2020-12-13 19:10:43 +0200

    feat: get and post handlers

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -1,9 +1,13 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let projectData = {test: 'hello'};
 
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require ('cors');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +15,39 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+
+// Running the server
+const port = 8000;
+app.listen(port, listenCallback);
+
+function listenCallback() {
+    console.log(`Server is Running ......`);
+}
+
+
+// get stored weather data
+app.get('/getWeatherEndPoint', getWeatherDataCallback);
+
+function getWeatherDataCallback(request, response) {
+    response.send(projectData);
+}
+
+
+
+
+// post request to save weather data
+app.post('/addWeatherData', addWeatherDataCallback);
+
+function addWeatherDataCallback (request, responses) {
+    projectData = request.body;
+    console.log(projectData);
+}
+
+
